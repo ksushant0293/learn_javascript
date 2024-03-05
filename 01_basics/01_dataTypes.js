a0df3dc2756ca8f68009bec4d079ef73151cf4f7
sushant kumar <120014761+ksushant0293@users.noreply.github.com>
2024-03-05 06:21:16 +0000

    added  datatypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3 + 3); // we are using node js not browser
+
+//console.log(3 + 3) console.log("sushant"); // code readability should be high
+
+let name = "sushant";
+let age = 31;
+let isLoggedIn = false;
+
+// number => 2 to power 53
+//bigint
+//string
+//boolean => true/false
+//null =>standalone value 
+//undefined =>
+// symbol => unique
+
+//object
+
+console.log(typeof "sushannt");
+console.log(typeof age);
+console.log(typeof null);  //object
+console.log(typeof undefined); //undefined
