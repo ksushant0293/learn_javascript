17474d230aa1f3daabf874fdfcbc61d26a6fb89c
sushant kumar <120014761+ksushant0293@users.noreply.github.com>
2024-03-05 14:08:50 +0000

    added comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log (2 == 1);
+// console.log(2 != 1);
+
+
+console.log("2" > 1); // here "2"(string) is converted into number
+console.log("02" > 1); // here "2"(string) is converted into number
+
+console.log(null > 0); // don't use this dtype of converstion
+console.log(null < 0);
+console.log(null >= 0);
+console.log(null <= 0);
+
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === strict check( checks value as well as data types)
+console.log("2" === 2);
