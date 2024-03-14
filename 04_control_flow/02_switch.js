bc44311705b9d65b7eb38f9b57da5e69a324e78e
sushant kumar <120014761+ksushant0293@users.noreply.github.com>
2024-03-14 14:03:20 +0000

    learned switch cases

--- 04_control_flow/02_switch.js
+++ 04_control_flow/02_switch.js
@@ -7,23 +7,41 @@
 //         break;
 // }
 
-const month = 3;
+const month = "jan";
+
+// switch (month) {
+//     case 1:
+//         console.log("january");
+//         break;
+//     case 2:
+//         console.log("febuary");
+//         break;
+//     case 3:
+//         console.log("march");
+//         break;
+//     case 4:
+//         console.log("april");
+//         break;
+//     default:
+//         console.log("default case match");
+//         break;
+// }
 
 switch (month) {
-    case 1:
-        console.log("january");
+    case "jan":
+        console.log("jan")
         break;
-    case 2:
-        console.log("febuary");
+    case "feb":
+        console.log("feb")
         break;
-    case 3:
-        console.log("march");
+    case "march":
+        console.log("march")
         break;
-    case 4:
-        console.log("april");
+    case "april":
+        console.log("april")
         break;
+
     default:
-        console.log("default case match");
+        console.log("default case match")
         break;
 }
-
